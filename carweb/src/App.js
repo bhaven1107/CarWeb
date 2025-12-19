b5df92e3277b260d4b0e191185b262ccb6aa9c1f
Sameer Gohil 007 <sameeryygg277@gmail.com>
2025-12-19 23:10:54 +0530

    registration form

--- carweb/src/App.js
+++ carweb/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Reg from './pages/registration & Login/Registration/reg.jsx';
 
 const App = () => {
   return (
-    <div>
-      Hello Car Web!
-    </div>
+    <Reg />
   )
 }
 
