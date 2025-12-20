d4fbe401eee09379dff8eed8346569bbdc75040e
Sameer Gohil 007 <sameeryygg277@gmail.com>
2025-12-20 12:19:42 +0530

    login page frontend

--- carweb/src/App.js
+++ carweb/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Reg from './pages/registration & Login/Registration/reg.jsx';
+import Log from './pages/registration & Login/Login/log.jsx';
 
 const App = () => {
   return (
+  <>
     <Reg />
+    <Log />
+  </>
   )
 }
 
