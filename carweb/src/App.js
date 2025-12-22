8034f2da11eccd17cc2d69ea39daf9b9ba0520bf
Sameer Gohil 007 <sameeryygg277@gmail.com>
2025-12-22 17:29:07 +0530

    Navbar not compelete

--- carweb/src/App.js
+++ carweb/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AppRoutes from './Routing/CarRoutes.jsx';
+import Navbar from './Component/header/Navbar.jsx';
 
 const App = () => {
   return (
-    <div>
-      <AppRoutes/>
-    </div>
+    <>
+      {/* <AppRoutes/> */}
+      <Navbar />
+    </>
   )
 }
 
