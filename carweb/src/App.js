7bf05341d693f596633e9cb03c25ba95a764fd77
Sameer Gohil 007 <sameeryygg277@gmail.com>
2025-12-24 10:31:53 +0530

    Revert "slider"
    This reverts commit d8ad7b222a07b8d27dc4e71e62d8d85993485115.

--- carweb/src/App.js
+++ carweb/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import AppRoutes from './Routing/CarRoutes.jsx';
 import Navbar from './Component/header/Navbar.jsx';
-import Home from './Component/Home/Home.jsx';
 
 const App = () => {
   return (
     <>
       {/* <AppRoutes/> */}
       <Navbar />
-      <Home />
     </>
   )
 }
