ab3fd132f852cc8c4d37ccd5b83698fa143f65b6
Sameer Gohil 007 <sameeryygg277@gmail.com>
2025-12-30 10:57:07 +0530

    Footer Create

--- carweb/src/Component/Home/Home.jsx
+++ carweb/src/Component/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
 const Home = () => {
 
@@ -8,6 +9,8 @@ const Home = () => {
     return (
         <>
             <Header />
+            <Footer />
+
         </>
     )
 }
--- carweb/src/Component/footer/footer.jsx
+++ carweb/src/Component/footer/footer.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
-const footer = () => {
+const Footer = () => {
     return (
-        <>
+        <footer className="footer">
+            <div className="footer-container">
 
-        </>
-    )
-}
+                {/* Brand Section */}
+                <div className="footer-brand">
+                    <h2>CarX Elite</h2>
+                    <p>
+                        Experience luxury, performance, and innovation.
+                        Your destination for premium cars.
+                    </p>
+                </div>
 
-export default footer;
+                {/* Quick Links */}
+                <div className="footer-links">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li>Home</li>
+                        <li>Cars</li>
+                        <li>Brands</li>
+                        <li>Services</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+
+                {/* Brands */}
+                <div className="footer-links">
+                    <h3>Luxury Brands</h3>
+                    <ul>
+                        <li>BMW</li>
+                        <li>Mercedes-Benz</li>
+                        <li>Audi</li>
+                        <li>Porsche</li>
+                        <li>Lamborghini</li>
+                    </ul>
+                </div>
+
+                {/* Social Media */}
+                <div className="footer-social">
+                    <h3>Follow Us</h3>
+                    <div className="social-icons">
+                        <FaFacebookF />
+                        <FaInstagram />
+                        <FaTwitter />
+                        <FaYoutube />
+                    </div>
+                </div>
+
+            </div>
+
+            {/* Bottom */}
+            <div className="footer-bottom">
+                © {new Date().getFullYear()} CarX Elite. All Rights Reserved.
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
